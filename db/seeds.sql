16541b7d978acaafc8dff76c4963b495e874f899
Craig Kaley <craig@caucusroom.com>
2020-06-28 18:16:12 -0600

    Work In Progress

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapano Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Barbeque Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jack Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teds Montana Grill Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cherry Criket Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hop Doddy Cheese Burger', false);
